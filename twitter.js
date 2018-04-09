b3c07a46b682ae9224103789bea29829eea38309
Molly Burns <Molly@Kathryns-MBP.attlocal.net>
2018-04-09 13:30:18 -0400

    spacing

--- twitter.js
+++ twitter.js
@@ -18,9 +18,10 @@ function tweets() {
             if (!error) {
                 //for loop to go through these twitter responses
                 for (var i = 0; i < tweets.length; i++) {
-                    console.log(tweets[i].created_at);
                     console.log("");
+                    console.log(tweets[i].created_at);
                     console.log(tweets[i].text);
+                    
                   }
 
 
